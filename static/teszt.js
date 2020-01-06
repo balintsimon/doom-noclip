2570bc99c5f58f99873213b7ba2e3274b85795a2
dr. Simon Bálint <balladin0@gmail.com>
2020-01-06 14:33:00 +0100

    Add sound play

--- static/teszt.js
+++ static/teszt.js
@@ -1,19 +1,25 @@
 console.log('JE')
+
+var snd = new Audio("static/dspistol.wav");
+
+
 const body = document.getElementById('teszt')
 body.addEventListener('mousedown',function () {
     body.style.backgroundColor = 'rgba(255,0,255,255)'
-    document.getElementById('gun').setAttribute('src', 'static/xUmdZpU.gif')
+    document.getElementById('gun').setAttribute('src', 'static/xUmdZpU.gif');
+    snd.play();
+    console.log('sound');
 },true)
 
 body.addEventListener('mouseup',function () {
     document.getElementById('gun').setAttribute('src', 'static/xUmdZpU.png')
-
+    snd.pause();
 },true)
 
 window.onmousemove = function (e) {
     var x = e.clientX,
         y = e.clientY;
-    document.getElementById('hello').innerText = x
+    document.getElementById('hello').innerText = x;
 
     if ( x > 370 && x < 1408 ){
     document.getElementById('gun').style.left = (x-370) + 'px';
