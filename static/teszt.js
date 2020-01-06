44c10c1cecfdf044bfac4bb629cca131c38b1b66
dr. Simon Bálint <balladin0@gmail.com>
2020-01-06 15:34:23 +0100

    add music play and stop with button press

--- static/teszt.js
+++ static/teszt.js
@@ -1,14 +1,14 @@
+console.log('JE');
 
-console.log('JE')
-
-var snd = new Audio("static/dspistol.wav");
-const body = document.getElementById('teszt')
+var snd = new Audio("static/gun.mp3");
+const body = document.getElementById('teszt');
 
 
 body.addEventListener('mousedown',function () {
-    body.style.backgroundColor = 'rgba(255,0,255,255)'
-
-    document.getElementById('gun').setAttribute('src', 'static/gun_1.gif')
+    body.style.backgroundColor = 'rgba(255,0,255,255)';
+    document.getElementById('gun').setAttribute('src', 'static/gun_1.gif');
+    snd.loop = true;
+    snd.play();
 },true)
 
 body.addEventListener('mouseup',function () {
@@ -27,3 +27,30 @@ window.onmousemove = function (e) {
     document.getElementById('gun').style.left = (x-370) + 'px';
     }
 };
+
+let music = new Audio("static/doom_gate_music.mp3");
+
+let play_music = function() {
+    music.loop = true;
+    music.play();
+};
+
+let stop_music = function() {
+    music.pause();
+};
+
+let music_button = document.getElementById("music-button");
+let music_is_playing = 0;
+
+music_button.addEventListener("click", function () {
+    if (music_is_playing === 1) {
+        music_is_playing = 0;
+        console.log(music_is_playing);
+        stop_music();
+    } else if (music_is_playing === 0) {
+        music_is_playing = 1;
+        console.log(music_is_playing);
+        play_music();
+    }
+});
+
