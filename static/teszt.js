c0efec3c450f2f99964a132e7752c781b862d45d
Petiazediter <pverebes74@gmail.com>
2020-01-06 14:13:48 +0100

    Merge conf.

--- static/teszt.js
+++ static/teszt.js
@@ -1,16 +1,20 @@
 console.log('JE')
 const body = document.getElementById('teszt')
+
+
+
 body.addEventListener('mousedown',function () {
     body.style.backgroundColor = 'rgba(255,0,255,255)'
-    document.getElementById('gun').setAttribute('src', 'static/xUmdZpU.gif')
+    document.getElementById('gun').setAttribute('src', 'static/gun_1.gif')
 },true)
 
 body.addEventListener('mouseup',function () {
-    document.getElementById('gun').setAttribute('src', 'static/xUmdZpU.png')
+    document.getElementById('gun').setAttribute('src', 'static/gun_1.png')
 
 },true)
 
 window.onmousemove = function (e) {
+
     var x = e.clientX,
         y = e.clientY;
     document.getElementById('hello').innerText = x
