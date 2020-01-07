25e576582c0511117da38b5a698402cacfff34b3
dr. Simon Bálint <balladin0@gmail.com>
2020-01-07 10:16:59 +0100

    gun sound and music with setInterveal

--- static/teszt.js
+++ static/teszt.js
@@ -1,36 +1,28 @@
 console.log('JE');
 
-var snd = new Audio("static/gun.mp3");
 const body = document.getElementById('teszt');
 
+//sound related stuff
+let pistol_sound = new Audio("static/dspistol.wav");
+let machinegun_sound = new Audio("static/gun2.mp3");
 
-body.addEventListener('mousedown',function () {
-    body.style.backgroundColor = 'rgba(255,0,255,255)';
-    document.getElementById('gun').setAttribute('src', 'static/gun_1.gif');
-    snd.loop = true;
-    snd.play();
-},true)
-
-body.addEventListener('mouseup',function () {
-    document.getElementById('gun').setAttribute('src', 'static/gun_1.png');
-    snd.pause();
-    snd.currentTime = 0;
-},true);
-
-
-window.onmousemove = function (e) {
-
-    var x = e.clientX,
-        y = e.clientY;
-    document.getElementById('hello').innerText = x;
-
-    if ( x > 370 && x < 1348 ){
-    document.getElementById('gun').style.left = (x-370) + 'px';
-    }
+let play_gun_sound = function(sound) {
+    setInterval(play_sound(sound), -1000);
 };
 
-let music = new Audio("static/doom_gate_music.mp3");
+let play_sound = function(sound) {
+    sound.loop = true;
+    sound.play();
+    sound.currentTime = 0;
+};
 
+let stop_gun_sound = function(sound) {
+    sound.pause();
+    sound.currentTime = 0;
+};
+
+//music related stuff
+let music = new Audio("static/doom_gate_music.mp3");
 let play_music = function() {
     music.loop = true;
     music.play();
@@ -58,3 +50,26 @@ music_button.addEventListener("click", function () {
     }
 });
 
+
+body.addEventListener('mousedown',function () {
+    body.style.backgroundColor = 'rgba(255,0,255,255)';
+    document.getElementById('gun').setAttribute('src', 'static/gun_1.gif');
+    play_gun_sound(machinegun_sound); //play gun sound
+},true);
+
+body.addEventListener('mouseup',function () {
+    document.getElementById('gun').setAttribute('src', 'static/gun_1.png');
+    stop_gun_sound(machinegun_sound); //stop gun sound
+},true);
+
+
+window.onmousemove = function (e) {
+
+    var x = e.clientX,
+        y = e.clientY;
+    document.getElementById('hello').innerText = x;
+
+    if ( x > 370 && x < 1348 ){
+    document.getElementById('gun').style.left = (x-370) + 'px';
+    }
+};
